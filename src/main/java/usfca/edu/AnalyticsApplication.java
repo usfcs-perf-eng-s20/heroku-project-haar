2bbedbe6f9ac532720ca093e5b343023743ef2f8
AlperOzdamar <alper.ozdamar@gmail.com>
2020-02-22 15:13:31 -0800

    Enable JPA Repositories

--- src/main/java/usfca/edu/AnalyticsApplication.java
+++ src/main/java/usfca/edu/AnalyticsApplication.java
@@ -3,13 +3,17 @@ package usfca.edu;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import usfca.edu.persistence.EdrRepository;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableJpaRepositories(basePackages="usfca.edu")
+@EntityScan(basePackages="usfca.edu")
 public class AnalyticsApplication {
 
     public static void main(String[] args) {
