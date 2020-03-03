2e1f6fa26beff11501aee538dbe2db7b618aa867
Allison Wong <azywong@gmail.com>
2020-03-03 12:30:57 -0800

    adding support for multiple regenerated timestamps

--- db-seed/seed.js
+++ db-seed/seed.js
@@ -17,7 +17,12 @@ function getRandomData() {
 	let responseCode = config.responseCode[getRandom(0, config.responseCode.length)]
 	let success = config.success[getRandom(0, config.success.length)]
 	let username = config.username[getRandom(0, config.username.length)]
-	let timestamp = Math.round(new Date().getTime()/1000);
+	let timestamp = Math.round(new Date().getTime());
+
+	if(config.timestamp.length > 0) {
+		timestamp = config.timestamp[getRandom(0, config.timestamp.length)] + getRandom(0, 30000)
+		timestamp = timestamp * 1000
+	}
 
 	let data = {
 		"method": config.services[service_idx].requests[request_idx].method,
@@ -26,7 +31,7 @@ function getRandomData() {
 		"responseCode": responseCode,
 		"serviceName": config.services[service_idx].name,
 		"success": success,
-		"timestamp": timestamp,
+		"timestamp": timestamp.toString(),
 		"username": username
 	}
 
@@ -36,7 +41,6 @@ function getRandomData() {
 for (let i = 0; i < request_count; i++) {
 
 	let data = JSON.stringify(getRandomData());
-
 	let options = {
 	  hostname: config.hostname,
 	  port: 443,
@@ -47,7 +51,7 @@ for (let i = 0; i < request_count; i++) {
 	    'Content-Length': data.length
 	  }
 	}
-
+	console.log(data)
 	const req = https.request(options, (res) => {
 		responses.push(res);
 		completed_requests++;
