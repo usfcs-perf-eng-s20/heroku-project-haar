9172ad225e902c733aa6c91cb537105fdb8e8df7
Razan Said <rsaid@dons.usfca.edu>
2020-03-26 09:52:12 -0700

    update

---
+++ src/main/java/usfca/edu/json/model/ResponceForm.java
@@ -0,0 +1,23 @@
+package usfca.edu.json.model;
+
+public class ResponceForm {
+	boolean confirm;
+	String message;
+}
+
+
+public boolean getConfirm() {
+    return confirm;
+}
+
+public void setConfirm(boolean confirm) {
+    this.confirm = confirm;
+}
+
+public boolean getMessage() {
+    return message;
+}
+
+public void setMessage(boolean message) {
+    this.message = message;
+}
